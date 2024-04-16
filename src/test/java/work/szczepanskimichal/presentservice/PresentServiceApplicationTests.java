0ea4d7cee2e30e84eb295b5706340a54fc6f1154
michals <michal.m.szczepanski@gmail.com>
2024-04-16 10:40:09 +0200

    initial git commit

---
+++ src/test/java/work/szczepanskimichal/presentservice/PresentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package work.szczepanskimichal.presentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
