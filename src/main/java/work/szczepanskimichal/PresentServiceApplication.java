5e8184f6b2ee118cc56bb75c883cd046ede178eb
michals <michal.m.szczepanski@gmail.com>
2024-04-16 20:57:16 +0200

    config: 3 configure local, prod and test application configuration; 2 configure postgresql-presents connection for prod and local development; 1 bootstrap pom.xml with majority of required dependencies

--- src/main/java/work/szczepanskimichal/PresentServiceApplication.java
+++ src/main/java/work/szczepanskimichal/PresentServiceApplication.java
@@ -1,13 +1,11 @@
-package work.szczepanskimichal.presentservice;
+package work.szczepanskimichal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PresentServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PresentServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PresentServiceApplication.class, args);
+    }
 }
--- src/test/java/work/szczepanskimichal/PresentServiceApplicationTests.java
+++ src/test/java/work/szczepanskimichal/PresentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package work.szczepanskimichal.presentservice;
+package work.szczepanskimichal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PresentServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
